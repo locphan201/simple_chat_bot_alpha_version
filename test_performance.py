98d47f1dd6a8f92923a269b464403fe847032f0d
locphan201 <77631348+locphan201@users.noreply.github.com>
2022-01-11 11:13:10 +0700

    Adding a tester and testing log
    Add a tester for performance and save it to the testing_log.txt

---
+++ test_performance.py
@@ -0,0 +1,45 @@
+import time
+from datetime import datetime
+from dictionary import Vocabulary
+
+#-------------------------------------------------#
+#-This is a tester for performance and efficiency-#
+#-------------------------------------------------#
+
+# Times to loop
+iterator = 1000000
+# Block of codes needs to test
+def code_for_test():
+    s = Vocabulary()
+
+# Please don't change anything below
+def save_testing_log(prompt, average_time, note):
+    # datetime object containing current date and time
+    now = datetime.now()
+    dt_string = now.strftime("%H:%M:%S %d/%m/%Y")
+    with open('testing_log.txt', 'a') as file:
+        file.write(dt_string + '\n')
+        file.write(' - ' + prompt + '\n')
+        file.write(' - Iterator: ' + str(iterator) + '\n')
+        file.write(' - Average time: ' + str(average_time) + '\n')
+        file.write(' - Note: ' + note + '\n\n')
+
+
+
+def main():
+    prompt = input('Kind of test: ')
+    
+    t0 = time.time()
+    for i in range(iterator):
+        code_for_test()
+    t1 = time.time()
+    for i in range(iterator):
+        code_for_test()
+    t2 = time.time()
+
+    average = round(((t1-t0) + (t2-t1))/2, 3)
+    print(average)
+    note = input('Note after testing: ')
+    save_testing_log(prompt, average, note)
+
+main()
